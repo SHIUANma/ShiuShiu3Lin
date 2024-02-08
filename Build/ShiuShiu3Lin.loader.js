d2837da52e42cd69a0d15aa718aa8210aa802f75
SHIUANma <73394203+SHIUANma@users.noreply.github.com>
2024-02-08 19:47:49 +0800

    123

--- Build/ShiuShiu3Lin.loader.js
+++ Build/ShiuShiu3Lin.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91246872,
+    wasmFileSize: 91246881,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
